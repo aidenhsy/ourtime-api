f15ded332064931566e242072d03a260c0eccd9f
aidenhsy <aidenhsy@hotmail.com>
2024-11-04 13:45:00 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import * as cookieParser from 'cookie-parser';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   const config = new DocumentBuilder()
