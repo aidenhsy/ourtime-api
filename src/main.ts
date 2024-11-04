10917625ad4136a7212ddab60d107199e27b8a3d
aidenhsy <aidenhsy@hotmail.com>
2024-11-04 14:00:47 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
-import * as yaml from 'js-yaml';
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   app.setGlobalPrefix('api');
-  app.use(cookieParser());
+
   const config = new DocumentBuilder()
     .setTitle('OURTIME API')
     .setDescription('OURTIME API DOCUMENTATION')
@@ -23,28 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  fs.writeFileSync('./openapi.json', JSON.stringify(document, null, 2));
-
-  // Convert JSON document to YAML and write to file
-  const yamlDocument = yaml.dump(document);
-  fs.writeFileSync('./openapi.yaml', yamlDocument);
-
-  app
-    .getHttpAdapter()
-    .getInstance()
-    .get('/openapi.json', (req, res) => {
-      res.send(document);
-    });
-
-  // Create endpoint to serve YAML document
-  app
-    .getHttpAdapter()
-    .getInstance()
-    .get('/openapi.yaml', (req, res) => {
-      res.type('text/yaml');
-      res.send(yamlDocument);
-    });
-
-  await app.listen(3100, '0.0.0.0');
+  await app.listen(3100);
 }
 bootstrap();
